e5f5d4ccb7e5fb640ca70d4a286008de7257c51f
Arnav Surve <arnav@surve.dev>
2025-03-09 15:21:59 -0700

    database connection

---
+++ pkg/db/db.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"github.com/arnavsurve/gateway-registry/pkg/types"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func InitDB() (*gorm.DB, error) {
+	dsn := "host=localhost user=postgres password=postgres dbname=gateway_registry port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.AutoMigrate(&types.MCPService{}, &types.Capability{}, &types.Category{}, &types.MetadataItem{}); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
